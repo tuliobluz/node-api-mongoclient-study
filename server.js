4f330c06c534d663d0743ae64442c80ee054479d
Tulio Luz <tulio8395@yahoo.com.br>
2018-12-04 13:47:27 -0200

    back to mongo client

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // server.js
 const express = require('express');
-const mongoose = require('mongoose');
-
+const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-
-const config = require('./config/db');
+const db = require('./config/db');
 const app = express();
 const port = 8000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(
-    config.url,
-    { server: { auto_reconnect: true } }
-)
-
-var db = mongoose.connection;
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
+})
