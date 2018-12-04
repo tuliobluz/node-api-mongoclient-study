f96e315077638ee718f02ca9dd0df28b7609123b
Tulio Luz <tulio8395@yahoo.com.br>
2018-12-04 13:52:51 -0200

    POST Done

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,9 +1,14 @@
-// routes/note_routes.js
+// note_routes.js
 module.exports = function (app, db) {
-
-    app.post('/notes', (req, res) => {
-        // You'll create your note here.
-        console.log(req.body);
-        res.send('Hello')
-    });
+    const collection =
+        app.post('/notes', (req, res) => {
+            const note = { text: req.body.body, title: req.body.title };
+            db.collection('notes').insert(note, (err, result) => {
+                if (err) {
+                    res.send({ 'error': 'An error has occurred' });
+                } else {
+                    res.send(result.ops[0]);
+                }
+            });
+        });
 };
